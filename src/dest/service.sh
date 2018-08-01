6fffb3d24bad5e2fa9717b77b1b48ec484a10658
James Carpenter <jamesjcarpenter@gmail.com>
2018-08-01 23:31:54 +0100

    service updates

--- src/dest/service.sh
+++ src/dest/service.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env sh
+#
+# Rclone service
 
+# import DroboApps framework functions
 . /etc/service.subr
 
 framework_version="2.1"
-
 name="Rclone"
 version="1.42"
 description="Rclone is a command line program to sync files and directories to and from various cloud storage services"
@@ -11,13 +13,16 @@ depends=""
 webui="WebUI"
 
 prog_dir="$(dirname "$(realpath "${0}")")"
-daemon="${prog_dir}/bin/simple-app-daemon"
-pidfile="/tmp/DroboApps/${name}/pid.txt"
+daemon="${prog_dir}/bin/rclone"
+conffile="${prog_dir}/etc/rclone.conf"
+tmp_dir="/tmp/DroboApps/${name}"
+pidfile="${tmp_dir}/pid.txt"
+logfile="${tmp_dir}/log.txt"
 
 start() {
   # Start ${daemon}, ensure ${pidfile is created}, detach from parent
   process
-  ${daemon} --pidfile="${pidfile}" --daemon
+  ${daemon} --config ${conffile}
 }
 
 main "$@"
